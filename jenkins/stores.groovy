d98f31a1540d93fc75e27fc101bd7854c10253e8
MullamgiRama Reddy <rama.reddy.mullamgi@macys.com>
2023-11-30 14:54:49 +0530

    added groovy

---
+++ jenkins/stores.groovy
@@ -0,0 +1,20 @@
+pipeline {
+  agent any
+
+  parameters {
+    choice(name: 'env', choices: ['QA','UAT'])
+  } 
+    stage('Test') {
+      steps {
+        script {
+          def selectedEnv = params.env
+          
+      echo "Selected Environment: ${selectedEnv}"
+          bat "mvn clean verify -Ddriver=chrome -DEnv=${selectedEnv}"          
+     
+        }
+      }
+        
+    }
+  }
+}
