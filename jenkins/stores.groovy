9e92092395c1df6f4a0b861f7fcea6d7bf00d9f5
MullamgiRama Reddy <rama.reddy.mullamgi@macys.com>
2023-12-04 12:10:04 +0530

    added groovy

--- jenkins/stores.groovy
+++ jenkins/stores.groovy
@@ -20,16 +20,13 @@ pipeline {
       }       
    post
  	{  
-// 	    success   {       
-//             publishHTML([allowMissing: true, alwaysLinkToLastBuild: false, keepAll: true, reportDir: 'target/site/serenity/', reportFiles: 'index.html', reportName: 'Serenity Report', reportTitles: '', useWrapperFileDirectly: true])
-//              }
   always {
       publishHTML (target : [allowMissing: false,
         alwaysLinkToLastBuild: false,
         keepAll: true,
         reportDir: 'target/site/serenity',
         reportFiles: 'index.html',
-        reportName: 'my-test-serenity-bdd-reports',
+        reportName: 'serenity-reports',
         reportTitles: 'The DW Report']) 
     }
       }
